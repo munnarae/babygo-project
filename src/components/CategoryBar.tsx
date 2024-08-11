e9278e30f47da5e13b8d6e1f0152f58b511e7d51
munnarae <skfo789@naver.com>
2024-08-12 02:19:37 +0900

    [Feat] useCategoryStore + 카테고리 필터링 추가

--- src/components/CategoryBar.tsx
+++ src/components/CategoryBar.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { usePlaceStore } from '../store/usePlaceStore';
 
 const categories = ['애가PASS', '여름물놀이', '이번주핫플', '축제&공연', '키즈숙박', '전시·체험·관람', '키카·테마파크', 'YES키즈존', '자연과함께', '동물친구'];
 
 const CategoryBar: React.FC = () => {
+    const { filterByCategory } = usePlaceStore();
+
     return (
         <div className="fixed top-[64px] z-40 w-full max-w-xl overflow-x-scroll flex space-x-4 p-4 pt-10 bg-yellow-300 border-b border-gray-300 no-scrollbar">
+            <button
+                className="flex-shrink-0 bg-white border border-gray-300 rounded-xl px-4 py-2 text-center text-sm font-medium"
+                onClick={() => filterByCategory(null)} // 전체 보기
+            >
+                전체
+            </button>
             {categories.map((category, index) => (
-                <div key={index} className="flex-shrink-0 bg-white border border-gray-300 rounded-xl px-4 py-2 text-center text-sm font-medium">
+                <button key={index} className="flex-shrink-0 bg-white border border-gray-300 rounded-xl px-4 py-2 text-center text-sm font-medium" onClick={() => filterByCategory(category)}>
                     {category}
-                </div>
+                </button>
             ))}
         </div>
     );
